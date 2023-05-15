262c594a2894c33b8cd541b91a4f5f18a2d99048
Irzem-Ahmed5 <111817651+Irzem-Ahmed5@users.noreply.github.com>
2023-05-15 17:13:19 -0400

    Initializing project

---
+++ EngineOilChangeApp/app/src/main/java/com/example/engineoilchangeapp/ListFragment.java
@@ -0,0 +1,22 @@
+package com.example.engineoilchangeapp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class ListFragment extends Fragment {
+
+    public ListFragment() {
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+
+
+    }
+}
