8fc5268509e48f6f2993c1665706a33679035f22
Irzem-Ahmed5 <111817651+Irzem-Ahmed5@users.noreply.github.com>
2023-05-15 17:58:01 -0400

    Initialization and adding fragments

--- EngineOilChangeApp/app/src/main/java/com/example/engineoilchangeapp/ListFragment.java
+++ EngineOilChangeApp/app/src/main/java/com/example/engineoilchangeapp/ListFragment.java
@@ -2,20 +2,33 @@ package com.example.engineoilchangeapp;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.engineoilchangeapp.databinding.FragmentListBinding;
+
 public class ListFragment extends Fragment {
 
     public ListFragment() {
     }
 
-
+        FragmentListBinding binding;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+        binding = FragmentListBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        getActivity().setTitle("Engine Oil Changes");
 
 
     }
---
+++ StudentsGradesListApp/app/src/main/java/com/example/assessment4/StudentsFragment.java
@@ -0,0 +1,76 @@
+package com.example.assessment4;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+
+import com.example.assessment4.Models.DataServices;
+import com.example.assessment4.Models.Student;
+import com.example.assessment4.databinding.FragmentStudentsBinding;
+
+import java.util.ArrayList;
+
+public class StudentsFragment extends Fragment {
+
+
+    public StudentsFragment() {
+        // Required empty public constructor
+    }
+
+    FragmentStudentsBinding binding;
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
+        binding = FragmentStudentsBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    ArrayAdapter<Student> adapter;
+    ArrayList<Student> students = new ArrayList<>();
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        getActivity().setTitle("Students");
+
+        students.clear();
+        students = DataServices.getStudents();
+        adapter = new ArrayAdapter<>(getActivity(), android.R.layout.simple_list_item_1, students);
+        binding.listView.setAdapter(adapter);
+
+        binding.listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+                //send to student hist frag
+
+                mListener.sendSelectedStudent(students.get(position));
+            }
+        });
+    }
+    StudentsListener mListener;
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        mListener = (StudentsListener) context;
+    }
+
+    interface StudentsListener {
+        void sendSelectedStudent(Student student);
+    }
+
+
+
+
+
+
+
+}
